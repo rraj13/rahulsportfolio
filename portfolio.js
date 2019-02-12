01f7da9b85396f2f4eb60dcb37edcc515ba981a2
Rahul Raj <rahulrajshopping@icloud.com>
2019-02-12 13:15:53 -0500

    added js to allow for menu toggling

--- portfolio.js
+++ portfolio.js
@@ -1,62 +1,122 @@
-$("#about-me").on("click", function() {
-    if ($(".main-div").css("top") === "150px") {
-        $(".header").text("Nav");
-        $(".main-div").animate({top: "0px"});
-        $(".content").removeClass("hidden");
+let clicked = false;
+
+function resetMainText() {
+    $(".header").html("<p id='test'>" + "Hi. I'm Rahul." + "</p>");
+    $(".header").append("<p>" + "I build " + "<span>" + "web apps." + "</span>" + "</p>");
+}
+
+function moveMainUp () {
+    $(".header").html("<p>" + "Nav" + "</p>");
+    $(".main-div").css("margin-top", "0px");
+    $(".main-div").animate({top: 0});
+}
+
+function moveMainDown() {
+    $(".header").html("<p id='test'>" + "Hi. I'm Rahul." + "</p>");
+    $(".header").append("<p>" + "I build " + "<span>" + "web apps." + "</span>" + "</p>");
+    $(".main-div").css("margin-top", "-95px");
+    $(".main-div").animate({top: "50%"});
+
+}
+
+$("#about-me").on("click", function(){
+    clicked = true;
+
+    if (clicked && window.innerWidth >= 320 && window.innerWidth < 400 && ($(".about-me").attr("style") === "display: none;" || $(".about-me").attr("style") === undefined)) {
+        moveMainUp();
+        $(".portfolio").fadeOut();
+        $(".contact").fadeOut();
         $(".about-me").fadeIn();
-    } else {
-        $(".header").html("<p>" + "Hi. I'm Rahul." + "</p>");
-        $(".header").append("<p>" + "I build <span>web apps.</span>" + "</p>");
-        $(".main-div").animate({top: "150px"});
+    } else if (clicked && window.innerWidth >= 400 && window.innerWidth < 740 && ($(".about-me").attr("style") === "display: none;" || $(".about-me").attr("style") === undefined)) {
+        moveMainUp();
+        $(".portfolio").fadeOut();
+        $(".contact").fadeOut();
+        $(".about-me").fadeIn();
+    } else if (clicked && window.innerWidth >= 740 && window.innerWidth < 920 && ($(".about-me").attr("style") === "display: none;" || $(".about-me").attr("style") === undefined)) {
+        moveMainUp();
+        $(".portfolio").fadeOut();
+        $(".contact").fadeOut();
+        $(".about-me").fadeIn();
+    } else if (clicked && window.innerWidth >= 920 && ($(".about-me").attr("style") === "display: none;" || $(".about-me").attr("style") === undefined)) {
+        $(".header").html("<p>" + "Nav" + "</p>");        
+        $(".portfolio").fadeOut();
+        $(".contact").fadeOut();
+        $(".about-me").fadeIn();
+    } else if ($(".about-me").attr("style") === "display: block;" && window.innerWidth < 920) {
+        moveMainDown();
         $(".about-me").fadeOut();
-        $(".content").addClass("hidden");
+    } else if ($(".about-me").attr("style") === "display: block;" && window.innerWidth >= 920) {
+        resetMainText()
+        $(".about-me").fadeOut();
     }
-    
+
 });
 
 $("#portfolio").on("click", function() {
-    if ($(".main-div").css("top") === "150px") {
-        $(".header").text("Nav");
-        $(".main-div").animate({top: "0px"});
-        $(".content").removeClass("hidden");
-        $(".portfolio").fadeIn();
-    } else {
-        $(".header").html("<p>" + "Hi. I'm Rahul." + "</p>");
-        $(".header").append("<p>" + "I build <span>web apps.</span>" + "</p>");
-        $(".main-div").animate({top: "150px"});
-        $(".portfolio").fadeOut();
-        $(".content").addClass("hidden");
-    }
-});
-
-$("#resume").on("click", function() {
-    if ($(".main-div").css("top") === "150px") {
-        $(".header").text("Nav");
-        $(".main-div").animate({top: "0px"});
-        $(".content").removeClass("hidden");
-        $(".resume").fadeIn();
-    } else {
-        $(".header").html("<p>" + "Hi. I'm Rahul." + "</p>");
-        $(".header").append("<p>" + "I build <span>web apps.</span>" + "</p>");
-        $(".main-div").animate({top: "150px"});
-        $(".resume").fadeOut();
-        $(".content").addClass("hidden");
-    }
-});
-
-$("#contact").on("click", function() {
-    if ($(".main-div").css("top") === "150px") {
-        $(".header").text("Nav");
-        $(".main-div").animate({top: "0px"});
-        $(".content").removeClass("hidden");
-        $(".contact").fadeIn();
-    } else {
-        $(".header").html("<p>" + "Hi. I'm Rahul." + "</p>");
-        $(".header").append("<p>" + "I build <span>web apps.</span>" + "</p>");
-        $(".main-div").animate({top: "150px"});
+    clicked = true;
+   
+    if (clicked && window.innerWidth >= 320 && window.innerWidth < 400 && ($(".portfolio").attr("style") === "display: none;" || $(".portfolio").attr("style") === undefined)) {
+        moveMainUp();
+        $(".about-me").fadeOut();
         $(".contact").fadeOut();
-        $(".content").addClass("hidden");
-    }
+        $(".portfolio").fadeIn();
+    } else if (clicked && window.innerWidth >= 400 && window.innerWidth < 740 && ($(".portfolio").attr("style") === "display: none;" || $(".portfolio").attr("style") === undefined)) {
+        moveMainUp();
+        $(".about-me").fadeOut();
+        $(".contact").fadeOut();
+        $(".portfolio").fadeIn();
+    } else if (clicked && window.innerWidth >= 740 && window.innerWidth < 920 && ($(".portfolio").attr("style") === "display: none;" || $(".portfolio").attr("style") === undefined)) {
+        moveMainUp();
+        $(".about-me").fadeOut();
+        $(".contact").fadeOut();
+        $(".portfolio").fadeIn();
+    } else if (clicked && window.innerWidth > 920 && ($(".portfolio").attr("style") === "display: none;" || $(".portfolio").attr("style") === undefined)) {
+        $(".header").html("<p>" + "Nav" + "</p>");
+        $(".about-me").fadeOut();
+        $(".contact").fadeOut();
+        $(".portfolio").fadeIn();
+    } else if ($(".portfolio").attr("style") === "display: block;" && window.innerWidth < 920) {
+        moveMainDown();
+        $(".portfolio").fadeOut();
+    } else if ($(".portfolio").attr("style") === "display: block;" && window.innerWidth >= 920) {
+        resetMainText()
+        $(".portfolio").fadeOut();
+    } 
+});
+
+
+$("#contact").on("click", function(){
+    clicked = true;
+    
+
+    if (clicked && window.innerWidth >= 320 && window.innerWidth < 400 && ($(".contact").attr("style") === "display: none;" || $(".contact").attr("style") === undefined)) {
+        moveMainUp();
+        $(".portfolio").fadeOut();
+        $(".about-me").fadeOut();
+        $(".contact").fadeIn();
+    } else if (clicked && window.innerWidth >= 400 && window.innerWidth < 740 && ($(".contact").attr("style") === "display: none;" || $(".contact").attr("style") === undefined)) {
+        moveMainUp();
+        $(".portfolio").fadeOut();
+        $(".about-me").fadeOut();
+        $(".contact").fadeIn();
+    } else if (clicked && window.innerWidth >= 740 && window.innerWidth < 920 && ($(".contact").attr("style") === "display: none;" || $(".contact").attr("style") === undefined)) {
+        moveMainUp();
+        $(".portfolio").fadeOut();
+        $(".about-me").fadeOut();
+        $(".contact").fadeIn();
+    } else if (clicked && window.innerWidth >= 920 && ($(".contact").attr("style") === "display: none;" || $(".contact").attr("style") === undefined)) {
+        $(".header").html("<p>" + "Nav" + "</p>");
+        $(".portfolio").fadeOut();
+        $(".about-me").fadeOut();
+        $(".contact").fadeIn();
+    } else if ($(".contact").attr("style") === "display: block;" && window.innerWidth < 920) {
+        moveMainDown();
+        $(".contact").fadeOut();
+    } else if ($(".contact").attr("style") === "display: block;" && window.innerWidth >= 920) {
+        resetMainText()
+        $(".contact").fadeOut();
+    } 
+
 });
 
 
